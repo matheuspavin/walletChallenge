16b78f2b4afce8eeb3497cbe3101f33236484131
matheuspavin <pavinmatheus@gmail.com>
2018-02-06 18:48:56 -0200

    First commit

---
+++ src/walletChallenge/log.java
@@ -0,0 +1,53 @@
+package walletChallenge;
+
+import java.util.Date;
+
+public class log {
+	
+	private Date date;
+	private String ip;
+	private String request;
+	private String status;
+	private String userAgent;
+	
+	public log(Date date, String ip, String request, String status, String userAgent) {
+		super();
+		this.date = date;
+		this.ip = ip;
+		this.request = request;
+		this.status = status;
+		this.userAgent = userAgent;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getRequest() {
+		return request;
+	}
+	public void setRequest(String request) {
+		this.request = request;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getUserAgent() {
+		return userAgent;
+	}
+	public void setUserAgent(String userAgent) {
+		this.userAgent = userAgent;
+	}
+	
+}
---
+++ src/walletChallenge/parser.java
@@ -0,0 +1,5 @@
+package walletChallenge;
+
+public class parser {
+
+}
